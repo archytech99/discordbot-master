717449852fdc4dbda62c40845f1b36a292685e04
Arief BP <ariefbp27@gmai.com>
2019-07-26 03:09:03 +0700

    Initial Commit

---
+++ pm2.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [{
+      name        : 'discordbot',
+      script      : 'index.js',
+      log         : 'discordbot.log',
+      output      : 'NULL',
+      error       : 'NULL',
+      merge_logs  : true
+    }]
+  };
+  
