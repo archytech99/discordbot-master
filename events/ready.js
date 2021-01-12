52c299c3dce1f9625a17060a078fabb56604776a
ariefbp27 <ariefbp27@gmail.com>
2021-01-12 16:46:50 +0700

    add status for bot

--- events/ready.js
+++ events/ready.js
@@ -2,10 +2,10 @@ const config = require("../config")
 const log = require('fancy-log')
 
 module.exports = (client, message) => {
-    // console.log(`Logged in as ${client.user.tag}!`)
     log.info(`READY: Logged in as ${client.user.tag}!`)
     client.user.setStatus(`Made by ${config.author}`)
     client.user.setPresence({
+        status: 'dnd',
         game: {
             name: config.bot_prefix+"help",
             type: "LISTENING",
