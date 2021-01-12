5816f4dc59e60643abcb1d5b486c350b74c02cb8
ariefbp27 <ariefbp27@gmail.com>
2021-01-12 16:47:22 +0700

    modify member private message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,6 @@
 const config = require('../config')
 
 module.exports = (client, member) => {
-    member.send("Hallo **"+member+"**! Selamat bergabung di **"+config.server+" Server**\n\n**Read Me First:**```•Dilarang Spam Chat di semua Channel;\n•Dilarang spam chat yang mengandung unsur sara, rasis, dsb;\n•Saling menghormati sesama Member lain;\n•Dilarang menyebar link yang mengandung unsur phising;\n•Ada keluhan tentang server kami, segera laporkan pada Mods/Admin.```\nHave fun :tada::hugging:")
+    member.send("Hallo **"+member+"**! Selamat Bergabung**\n\n**Read Me First:**```•Dilarang Spam Chat di semua Channel;\n•Dilarang spam chat yang mengandung unsur sara, rasis, dsb;\n•Saling menghormati sesama Member lain;\n•Dilarang menyebar link yang mengandung unsur phising;\n•Ada keluhan tentang server kami, segera laporkan pada Admin/Moderator.```\nHave fun :tada::hugging:")
   }
   
